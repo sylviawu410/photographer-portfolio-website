d8341dbf2e6b0c01ad5257f3c86b7ddde903c54e
Sylvia <sylvia.wu.wmy410@gmail.com>
2024-01-10 19:13:21 +0800

    innit commit (added Google fonts dependency)

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+        <Header />
+        <Nuxt />
+        <Footer />
+    </div>
+</template>
+  
+<script>
+</script>
+
+<style>
+body {
+  font-family: Manrope;
+  background: var(--Dark-03, #070708);
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  flex-shrink: 0;
+  margin-block-start: 0;
+  margin-block-end: 0;
+}
+</style>
