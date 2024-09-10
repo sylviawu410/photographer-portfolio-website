e7a5627dd354a011bf13f58b935ff5395e0f3e8f
Sylvia <sylvia.wu.wmy410@gmail.com>
2024-09-10 19:52:07 +0800

    added footer's scroll behavior

---
+++ pages/portfolio-page.vue
@@ -0,0 +1,30 @@
+<template>
+    
+
+
+    <div id="Events" class="events-section"></div>
+</template>
+
+<script>
+export default{
+    mounted(){
+        this.scrollToSection();
+    },
+    methods:{
+        scrollToSection(){
+            const hash = this.$route.hash;
+            if (hash){
+                const element = document.getElementById(hash.substr(1));
+                if(element){
+                    element.scrollIntoView({behavior: 'smooth'})
+                }
+            }
+        }
+    },
+    watch:{
+        '$route'(){
+            this.scrollToSection();
+        }
+    }
+}
+</script>
